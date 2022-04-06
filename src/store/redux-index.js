5ce09ad30d6f51da885b29b98a10f3d4df219b70
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-04-06 17:53:33 +0500

    Made use of store in Counter component and dispatched actions

--- src/store/redux-index.js
+++ src/store/redux-index.js
@@ -1,4 +1,4 @@
-import redux from "redux";
+import redux, { createStore } from "redux";
 
 const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "INCREMENT") {
@@ -7,7 +7,7 @@ const counterReducer = (state = { counter: 0 }, action) => {
     };
   }
 
-  if (action.type === "INCREMENT") {
+  if (action.type === "DECREMENT") {
     return {
       counter: state.counter - 1,
     };
@@ -15,6 +15,6 @@ const counterReducer = (state = { counter: 0 }, action) => {
   return state;
 };
 
-const store = redux.createStore(counterReducer);
+const store = createStore(counterReducer);
 
 export default store;
