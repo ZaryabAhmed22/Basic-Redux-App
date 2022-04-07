0009b6d52a697b008eac3056e38d3bbe2d99666d
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-04-08 00:21:36 +0500

    Added a slice for authentication

--- src/store/redux-index.js
+++ src/store/redux-index.js
@@ -1,11 +1,15 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 import redux, { createStore } from "redux";
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
+
+const initialAuthState = {
+  isAuth: false,
+};
 
 const counterSlice = createSlice({
   name: "counter",
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -22,13 +26,30 @@ const counterSlice = createSlice({
   },
 });
 
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuth = true;
+    },
+    logout(state) {
+      state.isAuth = false;
+    },
+  },
+});
+
 //passing reducers to our store
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer,
+  },
 });
 
 //dispatching actions by action creator methods
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
 
